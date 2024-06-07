2608e82149f82f90e5ddc78b0d3d3e030b597cc7
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-07 16:31:01 +0200

    build: add logic with words

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,1 +1,18 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+// genero un numero random per utente
+
+
+
+// genero un numero random per PC
+
+
+
+// confronto i due numeri:
+
+    // se numero utente più alto -> scrivi "Utente ha vinto"
+
+
+
+    // altrimenti -> "Ha vinto il PC"
