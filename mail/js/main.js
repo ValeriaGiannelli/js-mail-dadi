5d961ecf14f956a638b83bd258dacbbb78c8515a
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-07 16:11:51 +0200

    feat: create array with mails, prompt for user mail and display in console the element array

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,18 +1,20 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
 // creao una lista fittizia di mail
-const mailList = ["geralt@RTCEncodedVideoFrame.com", "gandalf@contea.it", "doctor@who.org"];
+const mailList = ["geralt@rivia.com", "gandalf@contea.it", "doctor@who.org"];
+console.log(mailList);
 
 // chiedere alla persona di inserire la propria mail
-
-
+const mailUser = prompt("Inserisci la tua mail:");
+console.log("la mail che ha inserito l'utente è:", mailUser);
 
 // controllare che la sua mail sia nella lista:
 
-
-
     // estraggo i singoli elementi della lista
+for(i = 0; i < mailList.length; i++){
+    console.log(mailList[i]);
 
+}
 
 
     // confronto il primo con la mail della persona
