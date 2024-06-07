f6a64dda02f14a74aaf7f21bcadb73f843b11e97
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-07 15:16:46 +0200

    Mail - filo logico

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,1 +1,28 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+// creao una lista fittizia di mail
+const mailList = ["geralt@RTCEncodedVideoFrame.com", "gandalf@contea.it", "doctor@who.org"];
+
+// chiedere alla persona di inserire la propria mail
+
+
+
+// controllare che la sua mail sia nella lista:
+
+
+
+    // estraggo i singoli elementi della lista
+
+
+
+    // confronto il primo con la mail della persona
+
+
+    // se diverso, procedo con il prossimo
+
+
+    // se trovo un elemento simile chiudo la ricerca e stampo "benvenuto alla festa"
+
+
+
+    // se la mail non corrisponde a nessuno degli elementi allora stampa "Non sei invitato"
